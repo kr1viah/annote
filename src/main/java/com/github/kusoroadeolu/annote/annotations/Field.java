4bfb292fbdf91191b7597a6007aa1fcdac65b3e9
Kusoro Adeolu <kusoroadeolu@gmail.com>
2026-01-09 19:19:17 +0100

    lol forgot to add target annotation for fields

--- src/main/java/com/github/kusoroadeolu/annote/annotations/Field.java
+++ src/main/java/com/github/kusoroadeolu/annote/annotations/Field.java
@@ -5,7 +5,7 @@ import com.github.kusoroadeolu.annote.annotations.containers.Fields;
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 @Repeatable(Fields.class)
 public @interface Field {
     String name(); //Name of the field
