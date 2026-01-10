875c94bb1c2304e53efe57893d47ca076238e16d
Kusoro Adeolu <kusoroadeolu@gmail.com>
2026-01-10 15:22:14 +0100

    bump to v0.0.5 cause of build failure

--- src/main/java/com/github/kusoroadeolu/annote/statements/AnnotationParser.java
+++ src/main/java/com/github/kusoroadeolu/annote/statements/AnnotationParser.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.*;
 
-import static com.github.kusoroadeolu.annote.ExceptionSupplier.run;
+
 import static com.github.kusoroadeolu.annote.Type.fromString;
 import static java.util.Collections.sort;
 
